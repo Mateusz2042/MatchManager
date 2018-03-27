d3b25c8d87221f2d0288bafd8189106f2f0a89e3
Mateusz Przybylo <mateusz2042@gmail.com>
2018-03-27 22:48:00 +0200

    Kilka widoków i kontrolery (! wersje nie do końca działające)

--- MatchAppUI/Controllers/PlayerController.cs
+++ MatchAppUI/Controllers/PlayerController.cs
@@ -5,19 +5,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MatchAppUI.Models;
+using MatchManager.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using MongoDB.Bson.Serialization;
+using System.Net;
 
 namespace MatchAppUI.Controllers
 {
     public class PlayerController : Controller
     {
+        public class DeserializePlayers
+        {
+            public IEnumerable<Player> Players { get; set; }
+        }
+
         public IActionResult GetAllPlayers()
         {
-            return View();
+            return View(deserialize().Players);
         }
 
         public IActionResult CreatePlayer()
         {
             return View();
+        }
+
+        public IActionResult EditPlayer(string id)
+        {
+            return View(deserialize(id));
+        }
+
+        public IActionResult DeletePlayer(string id)
+        {
+            return View(deserialize(id));
+        }
+        
+        public IActionResult DetailsPlayer(string id)
+        {
+            return View(deserialize(id));
         }
 
         public IActionResult Index()
@@ -43,5 +68,39 @@ namespace MatchAppUI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region MyRegion
+
+        public DeserializePlayers deserialize()
+        {
+            var url = string.Format("http://localhost:53766/api/Player/");
+
+            // Syncronious Consumption
+            var syncClient = new WebClient();
+
+            var content = syncClient.DownloadString(url);
+
+            DeserializePlayers players = JsonConvert.DeserializeObject<DeserializePlayers>(content);
+
+            return players;
+        }
+
+        public Player deserialize(string id)
+        {
+            var url = string.Format("http://localhost:53766/api/Player/");
+
+            // Syncronious Consumption
+            var syncClient = new WebClient();
+
+            var content = syncClient.DownloadString(url);
+
+            DeserializePlayers players = JsonConvert.DeserializeObject<DeserializePlayers>(content);
+
+            var player = players.Players.Where(x => x.Id == id).FirstOrDefault();
+
+            return player;
+        }
+
+        #endregion
     }
 }
---
+++ MatchAppUI/Controllers/TeamController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using MatchManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MatchAppUI.Controllers
+{
+    public class TeamController : Controller
+    {
+        public class DeserializeTeams
+        {
+            public IEnumerable<Team> Teams { get; set; }
+        }
+
+        public IActionResult GetAllTeams()
+        {
+            return View(deserialize().Teams);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region MyRegion
+
+        public DeserializeTeams deserialize()
+        {
+            var url = string.Format("http://localhost:53766/api/Match/");
+
+            // Syncronious Consumption
+            var syncClient = new WebClient();
+
+            var content = syncClient.DownloadString(url);
+
+            DeserializeTeams teams = JsonConvert.DeserializeObject<DeserializeTeams>(content);
+
+            return teams;
+        }
+
+        #endregion
+    }
+}
